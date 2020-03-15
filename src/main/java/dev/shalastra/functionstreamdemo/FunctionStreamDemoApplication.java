7ffa69b387ce85e1e2ddc02b8217fdd887a6fe62
szymon Halastra <szymon.halastra@gmail.com>
2020-03-15 17:11:29 +0100

    Initialize the project

---
+++ src/main/java/dev/shalastra/functionstreamdemo/FunctionStreamDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.shalastra.functionstreamdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionStreamDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionStreamDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/shalastra/functionstreamdemo/FunctionStreamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shalastra.functionstreamdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionStreamDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
